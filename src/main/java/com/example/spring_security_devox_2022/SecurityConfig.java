bc1503217930cc807d095bdacb2ed991151b2103
Max Peshkov <maxpeshkovit@gmail.com>
2025-08-08 16:45:05 +0400

    Add User identification information, stored in memory

--- src/main/java/com/example/spring_security_devox_2022/SecurityConfig.java
+++ src/main/java/com/example/spring_security_devox_2022/SecurityConfig.java
@@ -5,7 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +25,14 @@ class SecurityConfig {
                 })
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    UserDetailsService usersDetails() {
+        return new InMemoryUserDetailsManager(Set.of(User.builder()
+                .username("max")
+                .password("{noop}pass123")
+                .build())
+        );
     }
 }
